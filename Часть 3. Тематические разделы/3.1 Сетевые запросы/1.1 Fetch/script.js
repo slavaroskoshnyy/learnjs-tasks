ec9fa76a515e1577a091f888fad4d26e244385d1
slavaroskoshnyy <slava.roskoshnyy@gmail.com>
2023-03-13 17:38:38 +0300

    1

--- Часть 3. Тематические разделы/3.1 Сетевые запросы/1.1 Fetch/script.js
+++ Часть 3. Тематические разделы/3.1 Сетевые запросы/1.1 Fetch/script.js
@@ -31,18 +31,7 @@
 		}
 	 
 		let results = await Promise.all(jobs);
-	 	console.log(results);
+	 	// console.log(results);
 		return results;
 	 }
 	 getUsers(['iliakan', 'remy', 'no.su'])
-
-	// describe("getUsers", function() {
-
-	// 	it("gets users from GitHub", async function() {
-	// 	  let users = await getUsers(['iliakan', 'remy', 'no.such.users']);
-	// 	  assert.equal(users[0].login, 'iliakan');
-	// 	  assert.equal(users[1].login, 'remy');
-	// 	  assert.equal(users[2], null);
-	// 	});
-	 
-	//  });
